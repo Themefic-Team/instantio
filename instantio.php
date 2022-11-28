b3c1ca84dd37b77e84c465426ea2e0fd6c3b4def
DESKTOP-BH99S7S\PC - Themefic <hemelsrgmts@gmail.com>
2022-11-28 16:26:13 +0600

    Ready for release

--- instantio.php
+++ instantio.php
@@ -8,8 +8,8 @@
  * Domain Path: /lang/
  * Author URI: https://themefic.com
  * Tags: woocommerce, direct checkout, floating cart, side cart, ajax cart, cart popup, ajax add to cart, one page checkout, single page checkout, fly cart, mini cart, quick buy, instant checkout, quick checkout, same page checkout, sidebar cart, sticky cart, woocommerce ajax, one click checkout, woocommerce one page checkout, direct checkout woocommerce, woocommerce one click checkout, woocommerce quick checkout, woocommerce express checkout, woocommerce simple checkout, skip cart page woocommerce, woocommerce cart popup, edit woocommerce checkout page, woocommerce direct checkout
- * Version: 2.6.0
- * Tested up to: 6.0.2
+ * Version: 2.5.13
+ * Tested up to: 6.1.1
  * Requires PHP: 7.2
  * WC tested up to: 6.9.4
  */
@@ -86,7 +86,7 @@ if ( !is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
 
 // Define INSTANTIO_VERSION.
 if ( ! defined( 'INSTANTIO_VERSION' ) ) {
-	define( 'INSTANTIO_VERSION', '2.6.0' );
+	define( 'INSTANTIO_VERSION', '2.5.13' );
 }
 
 /**
