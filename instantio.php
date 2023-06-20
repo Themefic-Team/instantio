2a8fe27e2b807265e950fc62a4bf5e2f34f077ca
kamrul07 <kamrul07@users.noreply.github.com>
2023-06-20 20:09:30 +0600

    Update instantio.php

--- instantio.php
+++ instantio.php
@@ -10,9 +10,9 @@
  * Tags: woocommerce, direct checkout, floating cart, side cart, ajax cart, cart popup, ajax add to cart, one page checkout, single page checkout, fly cart, mini cart, quick buy, instant checkout, quick checkout, same page checkout, sidebar cart, sticky cart, woocommerce ajax, one click checkout, woocommerce one page checkout, direct checkout woocommerce, woocommerce one click checkout, woocommerce quick checkout, woocommerce express checkout, woocommerce simple checkout, skip cart page woocommerce, woocommerce cart popup, edit woocommerce checkout page, woocommerce direct checkout
 
  * Version: 3.0.0
- * Tested up to: 6.1.1
+ * Tested up to: 6.2.2
  * Requires PHP: 7.2
- * WC tested up to: 7.2.2
+ * WC tested up to: 7.8.0
 **/
 
 // don't load directly
