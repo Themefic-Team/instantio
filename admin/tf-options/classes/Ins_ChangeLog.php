beadd08a5cd4021eae2d4518a69739ff0483651a
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-06-22 13:24:48 +0600

    == 22 June 2023 - 3.0.1 ==
    Added: Language File Added
    Fixed: Php undefine error issue fixed
    Fixed: Upsell and Cross-sell variable quick view position issue fixed
    Fixed: Cart Fly JavaScript undefined error issue fixed
    Fixed: Column break Issue fixed( Pro )

--- admin/tf-options/classes/Ins_ChangeLog.php
+++ admin/tf-options/classes/Ins_ChangeLog.php
@@ -29,7 +29,23 @@ $change = [
     ],
     // Update End
 
-    //
+    // Update Start
+    'v2' => [
+        'date'      => '22 June 2023',
+        'version'   => 'Version 3.0.1',
+        'changelog' => [
+            'Fixed'   =>   [
+                'Php undefine error issue fixed',
+                'Upsell and Cross-sell variable quick view position issue fixed',
+                'Cart Fly JavaScript undefined error issue fixed',
+                'Column break Issue fixed( Pro )',
+            ], 
+            'New'   =>   [
+                'Language File Added', 
+            ],
+        ]
+    ],
+    // Update End
 ]
 
 
