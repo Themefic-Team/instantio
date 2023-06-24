7529427850a5b594695c050f4766b7bf692b3b7b
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-06-24 16:05:01 +0600

    Update Ins_ChangeLog.php
    3.0.1 Changelog Updated

--- admin/tf-options/classes/Ins_ChangeLog.php
+++ admin/tf-options/classes/Ins_ChangeLog.php
@@ -11,6 +11,24 @@
 
 $change = [
     // Update Start
+    'v2' => [
+        'date'      => '24 June 2023',
+        'version'   => 'Version 3.0.1',
+        'changelog' => [
+            'Fixed'   =>   [
+                'Undefined issue',
+                'Upsell & Cross Sell issue ( Pro )',
+                'Variation product issue in Cartfly',
+                'Column break Issue fixed( Pro )',
+            ], 
+            'New'   =>   [
+                'Language File Added', 
+            ],
+        ]
+    ],
+    // Update End
+    
+    // Update Start
     'v1' => [
         'date'      => '20 June 2023',
         'version'   => 'Version 3.0.0',
@@ -25,24 +43,6 @@ $change = [
                 "Custom Icon option added",
                 "Multistep checkout process added",
             ], 
-        ]
-    ],
-    // Update End
-
-    // Update Start
-    'v2' => [
-        'date'      => '22 June 2023',
-        'version'   => 'Version 3.0.1',
-        'changelog' => [
-            'Fixed'   =>   [
-                'Php undefine error issue fixed',
-                'Upsell and Cross-sell variable quick view position issue fixed',
-                'Cart Fly JavaScript undefined error issue fixed',
-                'Column break Issue fixed( Pro )',
-            ], 
-            'New'   =>   [
-                'Language File Added', 
-            ],
         ]
     ],
     // Update End
