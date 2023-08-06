81c8fe3ed9008c133422efed53970778df98e1ef
MHemeHasan -ThemeficPc <hemelsrgmts@gmail.com>
2023-08-06 10:30:51 +0600

    Fixing

--- includes/templates/cart-modern.php
+++ includes/templates/cart-modern.php
@@ -166,7 +166,7 @@ do_action( 'woocommerce_before_cart' ); ?>
                             ?> 
                             
                         </div>
-                        <div class="ins-cart-item-total data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
+                        <div class="ins-cart-item-total" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
                             <?php
                                 echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // PHPCS: XSS ok.
                             ?>
