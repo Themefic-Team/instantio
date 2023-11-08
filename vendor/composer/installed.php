8d6559afdd73802dbd60324797a17b37b543fd45
Mhemelhasan <hemelsrgmts@gmail.com>
2023-11-08 16:28:16 +0600

    Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'themefic/instantio',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '457696e92fd4074ce8077414a3ae786dc719518c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'themefic/instantio' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '457696e92fd4074ce8077414a3ae786dc719518c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
