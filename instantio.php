e65041a60b560db0402adc759f3d036781172307
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2023-12-10 17:24:49 +0600

    = 3.1.10 - ( December 10, 2023 ) =
    = 3.1.10 - ( December 10, 2023 ) =
    
    - Fixed: Responsive issue. 
    - compatibility: Compatible with WooCommerce 8.3

--- instantio.php
+++ instantio.php
@@ -8,10 +8,10 @@
  * Domain Path: /lang/
  * Author URI: https://themefic.com
  * Tags: woocommerce, direct checkout, floating cart, side cart, ajax cart, cart popup, ajax add to cart, one page checkout, single page checkout, fly cart, mini cart, quick buy, instant checkout, quick checkout, same page checkout, sidebar cart, sticky cart, woocommerce ajax, one click checkout, woocommerce one page checkout, direct checkout woocommerce, woocommerce one click checkout, woocommerce quick checkout, woocommerce express checkout, woocommerce simple checkout, skip cart page woocommerce, woocommerce cart popup, edit woocommerce checkout page, woocommerce direct checkout
- * Version: 3.1.9
+ * Version: 3.1.10
  * Tested up to: 6.4
  * Requires PHP: 7.2
- * WC tested up to: 8.2.2
+ * WC tested up to: 8.3
 **/
 
 // don't load directly
@@ -32,7 +32,7 @@ class INSTANTIO {
 	private function define_constants() {
 		if ( ! defined( 'INSTANTIO_VERSION' ) ) { 
 
-			define( 'INSTANTIO_VERSION', '3.1.9' ); 
+			define( 'INSTANTIO_VERSION', '3.1.10' ); 
 
 		} 
 		define( 'INS_URL', plugin_dir_url( __FILE__ ) ); 
