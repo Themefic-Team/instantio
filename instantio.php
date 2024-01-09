17f451b2e6e0fed2842784a7e395663309c0f48d
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2024-01-09 11:31:36 +0600

    = 3.2.1 - ( January 09, 2023 ) =
    = 3.2.1 - ( January 09, 2023 ) =
    
    - Compatibility: Compatible With Tourfic Plugin

--- instantio.php
+++ instantio.php
@@ -9,7 +9,7 @@
  * Author URI: https://themefic.com
  * Tags: woocommerce, direct checkout, floating cart, side cart, ajax cart, cart popup, ajax add to cart, one page checkout, single page checkout, fly cart, mini cart, quick buy, instant checkout, quick checkout, same page checkout, sidebar cart, sticky cart, woocommerce ajax, one click checkout, woocommerce one page checkout, direct checkout woocommerce, woocommerce one click checkout, woocommerce quick checkout, woocommerce express checkout, woocommerce simple checkout, skip cart page woocommerce, woocommerce cart popup, edit woocommerce checkout page, woocommerce direct checkout
 
- * Version: 3.3.0
+ * Version: 3.2.1
  * Tested up to: 6.4
  * Requires PHP: 7.2
  * WC tested up to: 8.4
@@ -33,7 +33,7 @@ class INSTANTIO {
 	private function define_constants() {
 		if ( ! defined( 'INSTANTIO_VERSION' ) ) {
 
-			define( 'INSTANTIO_VERSION', '3.3.0' );
+			define( 'INSTANTIO_VERSION', '3.2.1' );
 
 
 		}
