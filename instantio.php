7ef14b1c342700edb79daf3a1dee8f303bb54e2a
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2024-08-20 14:24:44 +0600

    = 3.3.0 - ( August 20, 2024 ) =
    = 3.3.0 - ( August 20, 2024 ) =
    
    - New Feature: Mini Cart Drawer
    - Introduced shortcode for mini cart

--- instantio.php
+++ instantio.php
@@ -8,7 +8,7 @@
  * Domain Path: /lang/
  * Author URI: https://themefic.com
  * Tags: woocommerce cart, woocommerce checkout, woocommerce direct checkout, multistep checkout, woocommerce side cart
- * Version: 3.2.14
+ * Version: 3.3.0
  * Tested up to: 6.6
  * Requires PHP: 7.4
  * WC tested up to: 9.1.4
@@ -31,7 +31,7 @@ class INSTANTIO {
 	 */
 	private function define_constants() {
 		if ( ! defined( 'INSTANTIO_VERSION' ) ) {
-			define( 'INSTANTIO_VERSION', '3.2.14' );
+			define( 'INSTANTIO_VERSION', '3.3.0' );
 		}
 		define( 'INS_URL', plugin_dir_url( __FILE__ ) );
 		define( 'INS_INC_URL', INS_URL . 'includes' );
