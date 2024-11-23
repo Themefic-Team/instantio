e3f7cfa6af069bbed104e6e40f7d3e95fe646f04
Ashik384 <39790492+Ashik384@users.noreply.github.com>
2024-11-23 23:15:14 +0600

    = 3.3.9 – November 23, 2024 =
    = 3.3.9 – November 23, 2024 =
    
    - Compatibility: Resolved translation notice issue for compatibility with WordPress 6.7.1.

--- instantio.php
+++ instantio.php
@@ -8,7 +8,7 @@
  * Domain Path: /lang/
  * Author URI: https://themefic.com
  * Tags: woocommerce cart, woocommerce checkout, woocommerce direct checkout, multistep checkout, woocommerce side cart
- * Version: 3.3.8
+ * Version: 3.3.9
  * Tested up to: 6.7
  * Requires PHP: 7.4
  * WC tested up to: 9.4
@@ -34,7 +34,7 @@ class INSTANTIO {
 	 */
 	private function define_constants() {
 		if ( ! defined( 'INSTANTIO_VERSION' ) ) {
-			define( 'INSTANTIO_VERSION', '3.3.8' );
+			define( 'INSTANTIO_VERSION', '3.3.9' );
 		}
 		define( 'INS_URL', plugin_dir_url( __FILE__ ) );
 		define( 'INS_INC_URL', INS_URL . 'includes' );
