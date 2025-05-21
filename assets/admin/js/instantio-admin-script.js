83ded3ef8cda5f02ca698206650961887c89f903
extenddeveloper <extenddeveloper@gmail.com>
2025-05-21 12:57:31 +0600

    Added settings sidebar

--- assets/admin/js/instantio-admin-script.js
+++ assets/admin/js/instantio-admin-script.js
@@ -103,3 +103,61 @@
     });
 
 })(jQuery);
+
+jQuery(document).ready(function($) {
+    $('.plugin-button').not('.pro').on('click', function(e) {
+        e.preventDefault();
+
+        let button = $(this);
+        let action = button.data('action');
+        let pluginSlug = button.data('plugin');
+        let pluginFileName = button.data('plugin_filename');
+
+        if (!action || !pluginSlug) return;
+
+        let loader = button.find('.loader');
+        let originalText = button.clone().children().remove().end().text().trim();
+
+        if (action === 'install') {
+            button.contents().first().replaceWith('Installing..');
+        } else if (action === 'activate') {
+            button.contents().first().replaceWith('Activating..');
+        }
+
+        button.addClass('loading').prop('disabled', true);
+        loader.show();
+
+        $.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            data: {
+                action: 'ins_themefic_manage_plugin',
+                security: ins_admin_data.themefic_nonce,
+                plugin_slug: pluginSlug,
+                plugin_filename: pluginFileName,
+                plugin_action: action
+            },
+            success: function(response) {
+                button.removeClass('loading').prop('disabled', false);
+                loader.hide();
+
+                if (response.success) {
+                    if (action === 'install') {
+                        button.contents().first().replaceWith('Activate');
+                        button.data('action', 'activate').removeClass('install').addClass('activate');
+                    } else if (action === 'activate') {
+                        button.replaceWith('<span class="plugin-button plugin-status active">Activated</span>');
+                    }
+                } else {
+                    button.contents().first().replaceWith(originalText);
+                    alert('Error: ' + response.data);
+                }
+            },
+            error: function() {
+                button.contents().first().replaceWith(originalText).removeClass('loading').prop('disabled', false);
+                loader.hide();
+                alert('An error occurred. Please try again.');
+            }
+        });
+    });
+});
