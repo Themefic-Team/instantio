374cc837722c3e5d39b3fc506dc6ae563ef1fce6
extenddeveloper <extenddeveloper@gmail.com>
2025-08-17 11:47:15 +0600

    Fixed settings class pre defined variablewarnings

--- admin/tf-options/classes/Ins_TF_Settings.php
+++ admin/tf-options/classes/Ins_TF_Settings.php
@@ -5,11 +5,14 @@ defined( 'ABSPATH' ) || exit;
 if ( ! class_exists( 'Ins_TF_Settings' ) ) {
 	class Ins_TF_Settings {
 
-		public $option_id = null;
-		public $option_title = null;
-		public $option_icon = null;
+		public $option_id       = null;
+		public $option_title    = null;
+		public $option_icon     = null;
 		public $option_position = null;
 		public $option_sections = array();
+		public $pre_tabs        = array();
+		public $pre_fields      = array();
+		public $pre_sections    = array();
 
 		public function __construct( $key, $params = array() ) {
 			$this->option_id = $key;
