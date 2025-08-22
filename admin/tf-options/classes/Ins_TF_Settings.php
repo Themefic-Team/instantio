cd0000ba206774deafea1ed47cf2023770b2a851
extenddeveloper <extenddeveloper@gmail.com>
2025-08-22 11:22:51 +0600

    Revert "Added Fomo Banner"
    This reverts commit 414ab9a1c5f393f41bc888b8a1047e2dedbe9fd2.

--- admin/tf-options/classes/Ins_TF_Settings.php
+++ admin/tf-options/classes/Ins_TF_Settings.php
@@ -923,9 +923,6 @@ if ( ! class_exists( 'Ins_TF_Settings' ) ) {
 						</div>
 
 						<div class="instantio-sidebar-content">
-							<!-- promo banner  -->
-					 		<?php echo apply_filters('ins_dashboard_helper_banner', ''); ?>
-
 							<?php echo $this->tf_get_sidebar_plugin_list(); ?>
 
 							<div class="customization-quote">
